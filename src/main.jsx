c7422337c4289ef1c342abe0b3284cc0b8d80bea
Mii-Muu <spotinfotech.in@gmail.com>
2024-10-17 13:24:20 +0530

    added htaccess

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App.jsx';
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import './styles/vendors/menu.css';
+// import './styles/style.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,10 +28,15 @@ import 'swiper/css/navigation';
 import './styles/vendors/menu.css';
 import './styles/style.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// Redirection logic to always redirect to 'www.orbyte.ai'
+if (window.location.hostname === 'orbyte.ai') {
+  window.location.replace(`https://www.orbyte.ai${window.location.pathname}${window.location.search}`);
+} else {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
