eb82fef838555968b851990bf9e0146eee3e1ee8
Ansalin sagar J <121027740+Ansalin789@users.noreply.github.com>
2025-06-06 16:23:32 +0530

    Update About.jsx

--- src/pages/common/About.jsx
+++ src/pages/common/About.jsx
@@ -31,9 +31,9 @@ const About = () => {
           <Service />
         </div>
         
-        <div className='px-4 sm:px-6 md:px-8 lg:px-12'>
+{/*         <div className='px-4 sm:px-6 md:px-8 lg:px-12'>
           <Team />
-        </div>
+        </div> */}
       </main>
     </>
   );
